7a464d798fd821b09dbf649c09c323ca1d62b6e5
unknown <tshegofatsopromise362@gmail.com>
2025-08-13 09:48:19 +0200

    Created a button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import Styles from "./Button.module.css";
+
+type ButtonProps = {
+  text: string;
+  onClick: () => void;
+};
+
+export const Button = ({ text, onClick }: ButtonProps) => {
+  return <button onClick={onClick}>{text}</button>;
+};
